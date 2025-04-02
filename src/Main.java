b2748db87b2a0b87fd3d9618c40d263d9ed97120
Miha <shmihail85@yandex.ru>
2025-04-02 11:52:58 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author tolstoy=new Author("Lev","Tolstoy");
+        Book warAndPeace = new Book("warAndPeace",tolstoy,2001);
+        warAndPeace.setYear(2222);
+        System.out.println("warAndPeace = " + warAndPeace.getYear());
+        Author pushkin=new Author("Aleksandr","Pushkin");
+        Book skazkaOCareSaltane=new Book("SkazkaOCareSaltane",pushkin,1985);
     }
+
 }
